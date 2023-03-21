64e839e8d4d0976234aa59d8889b5d4e24d4c717
yulei <yulei@addcn.com>
2023-03-21 13:46:56 +0800

    feat: routing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  hooks: {
+    'pages:extend' (pages) {
+      console.log('pages: ', pages);
+      pages.push({
+        name: 'detail',
+        path: '/:type(rent|sale)/:id',
+        file: '@/pages/rent/[id].vue',
+      })
+
+    }
+  }
 
 })
